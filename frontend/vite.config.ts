5fa48c27bbbd703e5e9f058714562e70102e93e3
FallicoFunctions <nickf579@gmail.com>
2026-01-12 15:56:51 -0500

    fix caching issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+    dedupe: ['react', 'react-dom', 'react-router-dom'],
   },
   server: {
     port: 5176,
