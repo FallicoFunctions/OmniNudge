aa9f661281e3df23957c4d7f3722cc8c727586d9
FallicoFunctions <nickf579@gmail.com>
2026-05-27 23:33:02 -0400

    fix(backend): restore hub metadata on author posts

---
+++ backend/internal/models/platform_post_test.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/omninudge/backend/internal/database"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPlatformPostRepositoryGetByAuthorIncludesHubMetadata(t *testing.T) {
+	db, err := database.NewTest()
+	require.NoError(t, err)
+	t.Cleanup(db.Close)
+
+	ctx := context.Background()
+	require.NoError(t, db.Migrate(ctx))
+	require.NoError(t, database.ResetTestData(ctx, db))
+
+	userRepo := NewUserRepository(db.Pool)
+	user := &User{
+		Username:     fmt.Sprintf("author_%d", time.Now().UnixNano()),
+		PasswordHash: "test_hash",
+	}
+	require.NoError(t, userRepo.Create(ctx, user))
+
+	hubRepo := NewHubRepository(db.Pool)
+	hubTitle := "Readable Hub"
+	hubDesc := "Hub for author posts"
+	hub := &Hub{
+		Name:        fmt.Sprintf("authorhub_%d", time.Now().UnixNano()),
+		Title:       &hubTitle,
+		Description: &hubDesc,
+		CreatedBy:   &user.ID,
+	}
+	require.NoError(t, hubRepo.Create(ctx, hub))
+
+	postRepo := NewPlatformPostRepository(db.Pool)
+	post := &PlatformPost{
+		AuthorID: user.ID,
+		HubID:    &hub.ID,
+		Title:    "Normal hub-created post",
+	}
+	require.NoError(t, postRepo.Create(ctx, post))
+
+	posts, err := postRepo.GetByAuthor(ctx, user.ID, 10, 0)
+	require.NoError(t, err)
+	require.Len(t, posts, 1)
+	require.Equal(t, hub.ID, *posts[0].HubID)
+	require.Equal(t, hub.Name, posts[0].HubName)
+	require.NotNil(t, posts[0].Hub)
+	require.Equal(t, hub.Name, posts[0].Hub.Name)
+	require.NotNil(t, posts[0].Hub.Title)
+	require.Equal(t, hubTitle, *posts[0].Hub.Title)
+	require.NotNil(t, posts[0].HubDisplayTitle)
+	require.Equal(t, hubTitle, *posts[0].HubDisplayTitle)
+}
