661fb48d4510e0c142ad8099f5f808a0926337c4
FallicoFunctions <nickf579@gmail.com>
2026-06-14 14:18:55 -0400

    perf: lazy load Babylon runtime chunk

--- omnirave-babylon/vite.config.ts
+++ omnirave-babylon/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 2300,
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
