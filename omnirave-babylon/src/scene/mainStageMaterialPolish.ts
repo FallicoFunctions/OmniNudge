22570bd9604fa1e07e427c4516534bdec5b3bf98
FallicoFunctions <nickf579@gmail.com>
2026-07-01 23:56:48 -0400

    fix: darken grand arcade pearl colonnade

--- omnirave-babylon/src/scene/mainStageMaterialPolish.ts
+++ omnirave-babylon/src/scene/mainStageMaterialPolish.ts
@@ -5711,15 +5711,15 @@ function applyPlazaPaverGoldFiligreeOverride(material: PBRMaterial) {
 
 function applyHeroPortalPearlArcadeOverride(material: PBRMaterial) {
   material.albedoTexture = null;
-  material.albedoColor = new Color3(0.24, 0.26, 0.3);
+  material.albedoColor = new Color3(0.2, 0.22, 0.26);
   material.emissiveColor = new Color3(0.006, 0.01, 0.014);
   material.emissiveIntensity = 0.02;
   material.metallic = 0.02;
-  material.roughness = 0.84;
+  material.roughness = 0.88;
   material.clearCoat.isEnabled = true;
   material.clearCoat.intensity = 0.04;
   material.clearCoat.roughness = 0.62;
-  material.environmentIntensity = 0.14;
+  material.environmentIntensity = 0.1;
   material.metadata = {
     ...material.metadata,
     mainStageMaterialOverride: 'hero-portal-pearl-arcade',
