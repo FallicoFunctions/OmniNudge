1d983c60c1bc9ffb650fefec94c5ae948ffa932f
FallicoFunctions <nickf579@gmail.com>
2026-07-13 02:12:00 -0400

    fix: invert trackpad camera axes

--- omnirave-babylon/src/scene/createMainStageScene.ts
+++ omnirave-babylon/src/scene/createMainStageScene.ts
@@ -125,8 +125,8 @@ export async function createMainStageScene(engine: AbstractEngine) {
 
     event.preventDefault();
     cameraRig.orbit(
-      event.deltaX * TRACKPAD_CAMERA_YAW_SENSITIVITY,
-      -event.deltaY * TRACKPAD_CAMERA_PITCH_SENSITIVITY,
+      -event.deltaX * TRACKPAD_CAMERA_YAW_SENSITIVITY,
+      event.deltaY * TRACKPAD_CAMERA_PITCH_SENSITIVITY,
     );
   };
   const handleCameraPointerDown = (event: PointerEvent) => {
