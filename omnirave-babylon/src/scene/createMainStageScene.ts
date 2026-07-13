0398ad3a47a4944ad0c73b2ba1e21a8d6be9e360
FallicoFunctions <nickf579@gmail.com>
2026-07-13 02:14:25 -0400

    fix: flip trackpad camera x axis

--- omnirave-babylon/src/scene/createMainStageScene.ts
+++ omnirave-babylon/src/scene/createMainStageScene.ts
@@ -125,7 +125,7 @@ export async function createMainStageScene(engine: AbstractEngine) {
 
     event.preventDefault();
     cameraRig.orbit(
-      -event.deltaX * TRACKPAD_CAMERA_YAW_SENSITIVITY,
+      event.deltaX * TRACKPAD_CAMERA_YAW_SENSITIVITY,
       event.deltaY * TRACKPAD_CAMERA_PITCH_SENSITIVITY,
     );
   };
